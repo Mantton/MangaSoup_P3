c5db32c217ccf4f24f1d18ade591ab1bb2f647cb
Mantton <mantton.official@gmail.com>
2020-11-17 23:06:34 +0100

    PageView Changes

--- lib/Screens/Reader/DebugReader.dart
+++ lib/Screens/Reader/DebugReader.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:mangasoup_prototype_3/Components/PlatformComponents.dart';
 import 'package:mangasoup_prototype_3/Components/ReaderComponents.dart';
 import 'package:mangasoup_prototype_3/Models/ImageChapter.dart';
@@ -50,25 +52,109 @@ class _DebugReaderState extends State<DebugReader> {
 
   Future<bool> initializer;
   List<ImageChapter> loadedChapters = List();
-  bool vertical = false;
+  bool vertical = true;
+  int readerMode = 0;
 
   @override
   void initState() {
     super.initState();
     initializer = init(widget.selectedChapter);
+    _pageController = PageController();
+    _listScrollController = ScrollController();
   }
+
+  PageController _pageController;
+  ScrollController _listScrollController;
+  ScrollController _activeScrollController;
+  Drag _drag;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _listScrollController.dispose();
+    super.dispose();
+  }
+
+  void _handleDragStart(DragStartDetails details) {
+    if (_listScrollController.hasClients &&
+        _listScrollController.position.context.storageContext != null) {
+      final RenderBox renderBox = _listScrollController
+          .position.context.storageContext
+          .findRenderObject();
+      if (renderBox.paintBounds
+          .shift(renderBox.localToGlobal(Offset.zero))
+          .contains(details.globalPosition)) {
+        _activeScrollController = _listScrollController;
+        _drag = _activeScrollController.position.drag(details, _disposeDrag);
+        return;
+      }
+    }
+    _activeScrollController = _pageController;
+    _drag = _pageController.position.drag(details, _disposeDrag);
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    var scrollDirection = _activeScrollController.position.userScrollDirection;
+
+    if (_activeScrollController == _listScrollController &&
+        ((scrollDirection == ScrollDirection.reverse) &&
+                _activeScrollController.offset.roundToDouble() >=
+                    _activeScrollController.position.maxScrollExtent
+                        .roundToDouble() ||
+            (scrollDirection == ScrollDirection.forward) &&
+                _activeScrollController.offset < 0)) {
+      _activeScrollController = _pageController;
+      _drag?.cancel();
+      _drag = _pageController.position.drag(
+          DragStartDetails(
+              globalPosition: details.globalPosition,
+              localPosition: details.localPosition),
+          _disposeDrag);
+    }
+    _drag?.update(details);
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    _drag?.end(details);
+  }
+
+  void _handleDragCancel() {
+    _drag?.cancel();
+  }
+
+  void _disposeDrag() {
+    _drag = null;
+  }
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  int _page = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text("Test Reader"),
+        title: Text("Test Reader : $_page"),
         actions: [
           IconButton(
             icon: Icon(Icons.rotate_left_rounded),
             onPressed: () {
               setState(() {
                 vertical = !vertical;
+                print(vertical);
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.phone_android),
+            onPressed: () {
+              setState(() {
+                if (readerMode == 1) {
+                  readerMode = 0;
+
+                  print("PageView");
+                } else
+                  readerMode = 1;
               });
             },
           )
@@ -97,7 +183,7 @@ class _DebugReaderState extends State<DebugReader> {
   }
 
   Widget home() {
-    return Container(child: pageView());
+    return Container(child: readerMode == 1 ? testOne() : fromGit());
   }
 
   Widget testOne() {
@@ -143,49 +229,68 @@ class _DebugReaderState extends State<DebugReader> {
     );
   }
 
-  Widget pageView() {
-    return PageView(
-      children: loadedChapters
-          .map(
-            (chapter) =>
-            PageView(
-              scrollDirection: vertical ? Axis.vertical : Axis.horizontal,
-
-              // shrinkWrap: true,
-              children: chapter.images
-                  .map(
-                    (image) =>
-                    Center(
-                      child: SingleChildScrollView(
-
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              // padding: EdgeInsets.all(10),
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width,
-                              child: ReaderImage(
-                                link: image,
-                                referer: chapter.referer,
-                                fit: BoxFit.fitWidth,
+  Widget fromGit() {
+    return RawGestureDetector(
+      gestures: <Type, GestureRecognizerFactory>{
+        VerticalDragGestureRecognizer:
+        GestureRecognizerFactoryWithHandlers<VerticalDragGestureRecognizer>(
+                () => VerticalDragGestureRecognizer(),
+                (VerticalDragGestureRecognizer instance) {
+              instance
+                ..onStart = _handleDragStart
+                ..onUpdate = _handleDragUpdate
+                ..onEnd = _handleDragEnd
+                ..onCancel = _handleDragCancel;
+            }),
+      },
+      behavior: HitTestBehavior.opaque,
+      child: PageView(
+        scrollDirection: vertical ? Axis.vertical : Axis.horizontal,
+        physics: NeverScrollableScrollPhysics(),
+        children: loadedChapters
+            .map(
+              (chapter) =>
+              PageView(
+                controller: _pageController,
+                physics: NeverScrollableScrollPhysics(),
+                // preloadPagesCount: 2,
+                scrollDirection: vertical ? Axis.vertical : Axis.horizontal,
+                onPageChanged: (p) {
+                  setState(() {
+                    _page = p;
+                  });
+                },
+                children: chapter.images
+                    .map(
+                      (image) =>
+                      Center(
+                        child: SingleChildScrollView(
+                          controller: _listScrollController,
+                          physics: NeverScrollableScrollPhysics(),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                width: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width,
+                                child: ReaderImage(
+                                  link: image,
+                                  referer: chapter.referer,
+                                  fit: BoxFit.fitWidth,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-              )
-                  .toList(),
-            ),
-      )
-          .toList(),
+                )
+                    .toList(),
+              ),
+        )
+            .toList(),
+      ),
     );
-  }
-
-  Widget testTwoo() {
-    return Container();
   }
 }
