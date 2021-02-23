916ab1ef0ed7c7f77dff8c714e915e22c4328f71
Mantton <mantton.official@gmail.com>
2021-02-23 18:15:49 -0500

    All Comics Refactor
    Signed-off-by: Mantton <mantton.official@gmail.com>

--- lib/Screens/Explore/AllComics.dart
+++ lib/Screens/Explore/AllComics.dart
@@ -159,93 +159,8 @@ class _AllComicsPageState extends State<AllComicsPage>
                                 onTap: () {
                                   showPlatformModalSheet(
                                     context: context,
-                                    builder: (_) => PlatformWidget(
-                                      material: (_, __) => ListView.builder(
-                                        shrinkWrap: true,
-                                        itemCount: sourceProvider
-                                            .source.sorters.length,
-                                        itemBuilder:
-                                            (BuildContext context, index) =>
-                                                ListTile(
-                                          title: Text(
-                                            sourceProvider.source.sorters[index]
-                                                ['name'],
-                                          ),
-                                          leading: Icon(
-                                            Icons.check,
-                                            color: (_sort['selector'] ==
-                                                    sourceProvider.source
-                                                            .sorters[index]
-                                                        ['selector'])
-                                                ? Colors.purple
-                                                : Colors.transparent,
-                                          ),
-                                          onTap: () {
-                                            setState(() {
-                                              _sort = sourceProvider
-                                                  .source.sorters[index];
-                                              _futureComics = _loadComics(
-                                                  sourceProvider
-                                                      .source.selector,
-                                                  _sort['selector'],
-                                                  1,
-                                                  {});
-                                              Navigator.pop(context);
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                      cupertino: (_, __) =>
-                                          CupertinoActionSheet(
-                                            title: Text(
-                                          "Sort by",
-                                        ),
-                                        cancelButton:
-                                            CupertinoActionSheetAction(
-                                          child: Text("Cancel"),
-                                          isDestructiveAction: true,
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                        ),
-                                        actions: List<
-                                            CupertinoActionSheetAction>.generate(
-                                          sourceProvider.source.sorters.length,
-                                          (index) => CupertinoActionSheetAction(
-                                            onPressed: () {
-                                              setState(() {
-                                                _sort = sourceProvider
-                                                    .source.sorters[index];
-                                                _futureComics = _loadComics(
-                                                    sourceProvider
-                                                        .source.selector,
-                                                    _sort['selector'],
-                                                    1,
-                                                    {});
-                                                Navigator.pop(context);
-                                              });
-                                            },
-                                            child: Row(
-                                              children: <Widget>[
-                                                Text(
-                                                  sourceProvider.source
-                                                      .sorters[index]['name'],
-                                                ),
-                                                Spacer(),
-                                                Icon(
-                                                  _sort['selector'] ==
-                                                          sourceProvider.source
-                                                                  .sorters[
-                                                              index]['selector']
-                                                      ? CupertinoIcons
-                                                          .check_mark
-                                                      : null,
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
+                                    builder: (_) =>
+                                        buildSortTypes(sourceProvider, context),
                                   );
                                 },
                               ),
@@ -280,6 +195,73 @@ class _AllComicsPageState extends State<AllComicsPage>
               );
             }
           }),
+    );
+  }
+
+  PlatformWidget buildSortTypes(
+      SourceNotifier sourceProvider, BuildContext context) {
+    return PlatformWidget(
+      material: (_, __) => ListView.builder(
+        shrinkWrap: true,
+        itemCount: sourceProvider.source.sorters.length,
+        itemBuilder: (BuildContext context, index) => ListTile(
+          title: Text(
+            sourceProvider.source.sorters[index]['name'],
+          ),
+          leading: Icon(
+            Icons.check,
+            color: (_sort['selector'] ==
+                    sourceProvider.source.sorters[index]['selector'])
+                ? Colors.purple
+                : Colors.transparent,
+          ),
+          onTap: () {
+            setState(() {
+              _sort = sourceProvider.source.sorters[index];
+              _futureComics = _loadComics(
+                  sourceProvider.source.selector, _sort['selector'], 1, {});
+              Navigator.pop(context);
+            });
+          },
+        ),
+      ),
+      cupertino: (_, __) => CupertinoActionSheet(
+        title: Text(
+          "Sort by",
+        ),
+        cancelButton: CupertinoActionSheetAction(
+          child: Text("Cancel"),
+          isDestructiveAction: true,
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: List<CupertinoActionSheetAction>.generate(
+          sourceProvider.source.sorters.length,
+          (index) => CupertinoActionSheetAction(
+            onPressed: () {
+              setState(() {
+                _sort = sourceProvider.source.sorters[index];
+                _futureComics = _loadComics(
+                    sourceProvider.source.selector, _sort['selector'], 1, {});
+                Navigator.pop(context);
+              });
+            },
+            child: Row(
+              children: <Widget>[
+                Text(
+                  sourceProvider.source.sorters[index]['name'],
+                ),
+                Spacer(),
+                Icon(
+                  _sort['selector'] ==
+                          sourceProvider.source.sorters[index]['selector']
+                      ? CupertinoIcons.check_mark
+                      : null,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
