1890659ff7c902d2195f55a6e5a55c6240fd02e7
Mantton <mantton.official@gmail.com>
2021-02-23 18:29:08 -0500

    refactoring
    Signed-off-by: Mantton <mantton.official@gmail.com>

--- lib/app/screens/reader/widgets/reached_end_page.dart
+++ lib/app/screens/reader/widgets/reached_end_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mangasoup_prototype_3/app/constants/fonts.dart';
 import 'package:mangasoup_prototype_3/app/data/database/models/comic.dart';
 import 'package:mangasoup_prototype_3/app/widgets/comic_collection_widget.dart';
