87c976e9a5613113f5b881012a5a761a7957aa5d
Mantton <mantton.official@gmail.com>
2021-03-14 16:25:25 -0400

    Queue
    Signed-off-by: Mantton <mantton.official@gmail.com>

--- lib/app/screens/downloads/screens/d_queue.dart
+++ lib/app/screens/downloads/screens/d_queue.dart
@@ -1,7 +1,9 @@
-
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:mangasoup_prototype_3/app/constants/fonts.dart';
 import 'package:mangasoup_prototype_3/app/data/database/database_provider.dart';
+import 'package:mangasoup_prototype_3/app/data/database/models/chapter.dart';
+import 'package:mangasoup_prototype_3/app/data/database/models/comic.dart';
 import 'package:mangasoup_prototype_3/app/screens/downloads/models/task_model.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:provider/provider.dart';
@@ -29,37 +31,65 @@ class _DownloadQueueState extends State<DownloadQueue> {
       List<int> keys = sorted.keys.toList();
       return Container(
         child: ListView.separated(
-            itemBuilder: (_, index) => ExpansionTile(
-                  title: Text(
-                    db.comics.firstWhere((e) => e.id == keys[index]).title,
-                  ),
-                  subtitle: Text(
-                    db.comics.firstWhere((e) => e.id == keys[index]).source,
-                  ),
-                  children: List.generate(
-                    sorted[keys[index]].length,
-                    (i) => ListTile(
-                      title: Text(db.chapters
-                          .firstWhere(
-                              (e) => e.id == sorted[keys[index]][i].chapterId)
-                          .title),
-                      trailing: CircularPercentIndicator(
-                        radius: 45.0,
-                        lineWidth: 3.0,
-                        percent: sorted[keys[index]][i].progress / 100,
-                        progressColor: Colors.purple,
-                        backgroundColor: Colors.grey[900],
-                        // fillColor: Colors.grey[900],
-                      ),
-                      onTap: () => print(sorted[keys[index]][i].status),
-                    ),
-                  ),
-                ),
-            separatorBuilder: (_, index) => SizedBox(
-                  height: 7,
-                ),
-            itemCount: keys.length),
+          itemBuilder: (_, index) => ComicDownloadBlock(
+            provider: db,
+            comic: db.comics.firstWhere((element) => keys[index] == element.id),
+            chapts: sorted[keys[index]],
+          ),
+          separatorBuilder: (_, index) => SizedBox(
+            height: 7,
+          ),
+          itemCount: keys.length,
+        ),
       );
     });
+  }
+}
+
+class ComicDownloadBlock extends StatelessWidget {
+  final DatabaseProvider provider;
+  final Comic comic;
+  final List<ChapterDownload> chapts;
+
+  const ComicDownloadBlock({Key key, this.provider, this.comic, this.chapts})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<ChapterData> chapterData = provider.chapters
+        .where((e) => chapts.map((k) => k.chapterId).contains(e.id))
+        .toList();
+    chapterData.sort(
+        (a, b) => b.generatedChapterNumber.compareTo(a.generatedChapterNumber));
+    return ExpansionTile(
+      title: Text(
+        comic.title,
+        style: notInLibraryFont,
+      ),
+      subtitle: Text(
+        comic.source,
+        style: TextStyle(
+          color: Colors.grey,
+        ),
+      ),
+      children: List.generate(
+        chapterData.length,
+        (i) {
+          var data = chapterData[i];
+          var x = chapts.firstWhere((element) => element.chapterId == data.id);
+          return ListTile(
+            title: Text(data.title),
+            trailing: CircularPercentIndicator(
+              radius: 45.0,
+              lineWidth: 3.0,
+              percent: x.progress / 100,
+              progressColor: Colors.purple,
+              backgroundColor: Colors.grey[900],
+              // fillColor: Colors.grey[900],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
